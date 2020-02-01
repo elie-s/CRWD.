a7612173e7501bf7d4cbf1373030438467792b3b
Lucas Kauff <l.kauff@rubika-edu.com>
2020-02-01 01:13:33 +0100

    Music Manager added + SFX Manager almost done

---
+++ CRWD/Assets/1_Scripts/General/SfxManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CRWD
+{
+    public class SfxManager : MonoBehaviour
+    {
+        [SerializeField] private AudioSource getBweep = default;
+        [SerializeField] private RandomSource nextPart = default;
+
+
+    }
+}
---
+++ CRWD/Assets/1_Scripts/LevelDesign/RandomSource.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CRWD
+{
+    public class RandomSource : MonoBehaviour
+    {
+        [SerializeField] private AudioSource source = default;
+        [SerializeField] private AudioClip[] clips = default;
+
+        public void Play()
+        {
+            if (!source || clips.Length == 0) return;
+
+            if (source.isPlaying) source.Stop();
+            source.clip = clips[Random.Range(0, clips.Length)];
+            source.Play();
+        }
+    }
+}
